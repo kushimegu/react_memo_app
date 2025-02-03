25ceabf9d39411fb913d678c661b3a446b7e4edc
Kushizaki <kushimegu288@gmail.com>
2025-02-03 17:41:43 +0900

    単一ファイルでメモCRUD機能の実装

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,99 @@
-import React from 'react'
+import { React, useState } from 'react';
+
 import './App.css';
 
-const MEMOS = [
-  {title: "メモ1", content: "メモの内容"},
-  {title: "メモ2", content: "メモ2の内容"},
-  {title: "メモ3", content: "メモ3の内容"}
-]
+export default function App() {
+  const [memos, setMemos] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+  const [contents, setContents] = useState('新規メモ');
 
-export default function App(){
+  function handleClick(id) {
+    setSelectedId(id);
+    const memo = memos.find((memo) => memo.id === id);
+    setContents(memo.contents.join('\n'));
+  }
+
+  function handleAdd() {
+    setContents('新規メモ');
+    setSelectedId(null);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newContents = contents.split('\n');
+    const newId = crypto.randomUUID();
+    setMemos((prevMemos) => {
+      const memoExists = prevMemos.some((memo) => memo.id === selectedId);
+      let updatedMemos;
+      if (memoExists) {
+        updatedMemos = prevMemos.map((memo) =>
+          memo.id === selectedId ? { ...memo, contents: newContents } : memo
+        );
+      } else {
+        updatedMemos = [...prevMemos, { id: newId, contents: newContents }];
+        setSelectedId(newId);
+      }
+      return updatedMemos;
+    });
+    if (!selectedId) {
+      localStorage.setItem(newId, JSON.stringify(newContents));
+    } else {
+      localStorage.setItem(selectedId, JSON.stringify(newContents));
+    }
+  };
+
+  function handleChange(e) {
+    setContents(e.target.value);
+  }
+
+  function handleDelete() {
+    localStorage.removeItem(selectedId);
+    if (selectedId) {
+      setMemos((prevMemos) => {
+        const updatedMemos = prevMemos.filter((memo) => memo.id !== selectedId);
+        return updatedMemos;
+      });
+    }
+  }
+
   return (
-  <ul>
-  {MEMOS.map((memo, index) => (
-    <li key={index}>{memo.title}</li>))}
-  </ul>)
+    <>
+      <div className="memoDetails">
+        <ul className="memoList">
+          {memos.map((memo) => (
+            <li key={memo.id}>
+              <div
+                onClick={() => {
+                  handleClick(memo.id);
+                }}
+              >
+                {memo.contents[0]}
+              </div>
+            </li>
+          ))}
+          <div className="addMemo" onClick={handleAdd}>
+            +
+          </div>
+        </ul>
+        <div className="selectedMemoBar">
+          <div className="selectedMemoDetail">
+            <form onSubmit={handleSubmit}>
+              <textarea
+                type="text"
+                name="memo"
+                value={contents}
+                onChange={handleChange}
+              />
+              <button type="submit" className="edit">
+                編集
+              </button>
+            </form>
+            <button type="delete" className="delete" onClick={handleDelete}>
+              削除
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
